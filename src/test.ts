07360ac12921d86d3f3553120552a2d03626b0fd
Tyler Hastings <tyler.hastings@terminus.com>
2017-04-11 19:58:00 -0400

    remove import

--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,5 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
-import 'core-js/shim';
+//import 'core-js/shim';
 
 import 'zone.js/dist/long-stack-trace-zone';
 import 'zone.js/dist/proxy.js';
